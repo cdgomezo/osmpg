99e4696aefb8927f4b07cd02391b5d96c586026f
Carlos Gómez-Ortiz <64776977+cdgomezo@users.noreply.github.com>
2022-10-03 20:21:53 +0200

    Create highways.lua

---
+++ highways.lua
@@ -0,0 +1,13 @@
+local tables = {}
+
+tables.highways = osm2pgsql.define_way_table('highways', {
+    { column = 'type', type = 'text' },
+    { column = 'lanes', type = 'int8'},
+    { column = 'geom', type = 'linestring', projection = 4326 },
+})
+
+function osm2pgsql.process_way(object)
+    if object.tags.highway then
+        tables.highways:add_row{ type = object.tags.highway }
+    end
+end
